8f513312e40aea448e3656b02e6faaf9c7a1a180
hannamenetie <hannamenetie@gmail.com>
2022-12-09 08:29:08 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
-		  *main - Entry
-		 *Return: Always 0
-		*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success) */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);)
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+
+
 }
