fcd82cdb3e0a272bc60db1e283733adb858f953e
hannamenetie <hannamenetie@gmail.com>
2022-12-12 15:35:13 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success) */
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
 	return (0);
-
 
 }
