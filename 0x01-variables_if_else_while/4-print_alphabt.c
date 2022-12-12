a897483deb527a4ca3b2b928df3e81b8ff4b1962
hannamenetie <hannamenetie@gmail.com>
2022-12-12 02:24:46 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - return alphabets but except e and q
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char dj = 'a';
+
+	while (dj <= 'z');
+	{
+		if ((dj != 'e') && (dj != 'q'))
+		{
+			putchar (dj);
+		}
+		dj++;
+	}
+	putchar('\n');
+	return (0);
+}
