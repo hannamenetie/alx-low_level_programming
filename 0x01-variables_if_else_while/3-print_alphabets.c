91e3594dd104dc68de2819a60641a4f57d47dc25
hannamenetie <hannamenetie@gmail.com>
2022-12-11 22:59:28 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - returns alphabets in lower case and upper case
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int dj = 'a';
+	int DJ = 'Z';
+
+	while (dj <= 'z')
+	{
+		putchar(dj);
+		dj++;
+	}
+	while (DJ <= 'Z')
+	{
+		putchar(DJ);
+		DJ++;
+	}
+
+	putchar('\n');
+	return (0);
+}
