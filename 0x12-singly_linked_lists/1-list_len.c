564acc6dce6199cba294db325a7e41690b20a9b9
hanna and valentine <hannamenetie@gmail.com>
2023-01-27 14:01:50 -0800

    1list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,19 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
+
 /**
- * list_len - returns the number of elements in a list.
- * @h: singly linked list.
- * Return: number of elements in the list.
+ * list_len - check the code for Holberton School students.
+ * @h: name of the list
+ * Return: the number of nodes.
  */
 size_t list_len(const list_t *h)
 {
-	size_t count;
+	int count = 0;
 
-	count = 0;
-	while (h != NULL)
+	while (h)
 	{
-		h = h->next;
 		count++;
+		h = h->next;
 	}
-
 	return (count);
 }
